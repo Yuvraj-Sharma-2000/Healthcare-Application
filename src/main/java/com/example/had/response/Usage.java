3799e536b2d1d381fa4736c607181be7f33d921f
Yuvraj Sharma <ys8484529@gmail.com>
2023-04-28 11:43:46 +0530

    Changes by hashir

---
+++ src/main/java/com/example/had/response/Usage.java
@@ -0,0 +1,49 @@
+package com.example.had.response;
+
+import java.util.UUID;
+
+public class Usage
+{
+    private UUID id;
+    String usageTime;
+    int count;
+
+    public Usage(UUID id, String usageTime, int count) {
+        this.id = id;
+        this.usageTime = usageTime;
+        this.count = count;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getUsageTime() {
+        return usageTime;
+    }
+
+    public void setUsageTime(String usageTime) {
+        this.usageTime = usageTime;
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+    public void setCount(int count) {
+        this.count = count;
+    }
+
+    @Override
+    public String toString() {
+        return "Usage{" +
+                "id=" + id +
+                ", usageTime='" + usageTime + '\'' +
+                ", count=" + count +
+                '}';
+    }
+}
--- src/main/java/com/example/had/service/DoctorService.java
+++ src/main/java/com/example/had/service/DoctorService.java
@@ -226,6 +226,65 @@ public class DoctorService {
         return false;
     }
 
+    public List<Usage> getUsage(UUID doctorid)
+    {
+        String email = "leo.nienow@yahoo.com";
+        try {
+//            List<User>  userList = userRepository.findByDoctor_IdAndDoctor_Email(doctorid, email);
+            List<Usage> usageList = new ArrayList<>();
+            List<User> userList = userRepository.findByDoctor_Id(doctorid);
+
+
+            int less_fifteen = 0;
+            int fifteen_thirty = 0;
+            int thirty_forty_five = 0;
+            int forty_five_sixty = 0;
+            int greater_sixty = 0;
+
+            System.out.println(userList);
+
+            for(User user : userList)
+            {
+                List<Timestamp> entries = user.getEntryTime();
+                List<Timestamp> exits = user.getExitTime();
+
+                for(int i=0; i<entries.size(); i++)
+                {
+                    Timestamp t1 = entries.get(i);
+                    Timestamp t2 = exits.get(i);
+
+                    long milliseconds = t1.getTime() - t2.getTime();
+                    int seconds = (int) milliseconds / 1000;
+                    int minutes = (seconds % 3600) / 60;
+
+                    if(minutes < 15)
+                        less_fifteen++;
+                    else if(minutes>=15 && minutes<30)
+                        fifteen_thirty++;
+                    else if(minutes>=30 && minutes<45)
+                        thirty_forty_five++;
+                    else if(minutes>=45 && minutes<60)
+                        forty_five_sixty++;
+                    else
+                        greater_sixty++;
+                }
+                usageList.add(new Usage(user.getId(),"<15 mins", less_fifteen));
+                usageList.add(new Usage(user.getId(),"15-30 mins", fifteen_thirty));
+                usageList.add(new Usage(user.getId(),"30-45 mins", thirty_forty_five));
+                usageList.add(new Usage(user.getId(),"45-60 mins", forty_five_sixty));
+                usageList.add(new Usage(user.getId(),">60 mins", greater_sixty));
+
+                return usageList;
+
+            }
+
+        }
+        catch (Exception e){
+            System.out.println(e.getMessage());
+        }
+        return null;
+    }
+
     public List<Severity> getSeverityList(UUID doctorId) {
         try {
             List<User> userList = userRepository.findByDoctor_Id(doctorId);
